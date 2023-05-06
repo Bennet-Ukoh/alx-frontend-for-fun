dcb032847ea68fa0be3181cf42809e1cb6a716d0
Bennet-Ukoh <bennetukoh@gmail.com>
2023-05-06 16:17:09 +0000

    code refactored

--- markdown2html.py
+++ markdown2html.py
@@ -10,23 +10,22 @@ import os.path
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.stderr
-        .write("Usage: ./markdown2html.py <inputfile> <outputfile>\n")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    README.md = sys.argv[1]
+    README.html = sys.argv[2]
 
-    if not os.path.isfile(input_file):
-        sys.stderr.write(f"Missing {input_file}\n")
+    if not os.path.isfile(README.md):
+        sys.stderr.write(f"Missing {README.md}\n")
         sys.exit(1)
 
-    with open(input_file, 'r') as f:
+    with open(README.md, 'r') as f:
         md = f.read()
 
     html = markdown.markdown(md)
 
-    with open(output_file, 'w') as f:
+    with open(README.html, 'w') as f:
         f.write(html)
 
     sys.exit(0)
