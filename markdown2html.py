c8a072cafef3aae5e87c993508ea2ed07c0a8e62
Bennet-Ukoh <bennetukoh@gmail.com>
2023-06-26 14:37:49 +0000

    more documentation added

--- markdown2html.py
+++ markdown2html.py
@@ -10,22 +10,28 @@ import sys
 import os.path
 
 if __name__ == "__main__":
+    # Check the number of command-line arguments
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
+    # Get the input and output file names from command-line arguments
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
+    # Check if the input file exists
     if not os.path.isfile(input_file):
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
 
+    # Read the content of the input file
     with open(input_file, 'r') as f:
         md = f.read()
 
+    # Convert Markdown to HTML
     html = markdown.markdown(md)
 
+    # Write the HTML content to the output file
     with open(output_file, 'w') as f:
         f.write(html)
 
